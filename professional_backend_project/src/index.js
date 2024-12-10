40f2cba9041a03fdd0f9a0ef526dc8a786f2f1ce
rio-aman <amprajapati08893.com>
2024-12-10 22:30:50 +0530

    doing dotenv config and also connecting to MongoDB Atlas

---
+++ professional_backend_project/src/index.js
@@ -0,0 +1,44 @@
+//database ke connection ke time to error ayengi he ayengi to two ways to prevent 
+// 1- use try catch 
+// 2 - use promises
+
+// and one more thing database are always in differnt country so time lagta hai database ke connect me to async and await must hai use karo 
+// require('dotenv').config({path: './env'})  // but this affect the code consistency so we write below code of dotenv 
+import dotenv from 'dotenv'
+
+// FIRST APPROACH (writing all things in this file for connection)
+/* 
+import express from 'express'
+const app = express()
+
+// function connectDB(){}  // ONE WAY 
+
+// IFF function 
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error)=> {
+            console.log("ERROR : ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR :",error)
+        throw error
+    }
+
+} )()
+*/
+
+// SECOND APPROACH  (writing all connections in db/db.js then importing in this file)
+
+import connectDB from './db/db.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
